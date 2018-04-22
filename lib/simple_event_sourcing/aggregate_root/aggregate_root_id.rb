6d361cba1bba4904c57d6b3042242d0deb468d60
Manel <malotor@gmail.com>
2018-04-23 00:25:31 +0200

    Change Aggregate Root Id test

--- lib/simple_event_sourcing/aggregate_root/aggregate_root_id.rb
+++ lib/simple_event_sourcing/aggregate_root/aggregate_root_id.rb
@@ -13,11 +13,31 @@ module SimpleEventSourcing
     def to_s
       @value
     end
+
+    def ==(other_id)
+      self.class == other_id.class &&
+        value == other_id.value
+    end
+    alias eql? ==
   end
 
   class UUIDAggregateRootId < AggregateRootId
-    def initialize
-      super(SecureRandom.uuid)
+    def initialize(value)
+      throw AggregateRootIdError unless valid? value
+      super(value)
+    end
+
+    def self.generate
+      new SecureRandom.uuid
+    end
+
+    private
+
+    def valid?(uuid)
+      uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+      uuid_regex =~ uuid.to_s.downcase
     end
   end
+
+  class AggregateRootIdError < StandardError; end
 end
--- spec/aggregate_root_id_spec.rb
+++ spec/aggregate_root_id_spec.rb
@@ -1,16 +1,18 @@
 require 'facets'
 
-RSpec.describe "Aggregate Root Id" do
-
+RSpec.describe 'Aggregate Root Id' do
   it 'is a valid UUID' do
-    aggregate_id = SimpleEventSourcing::UUIDAggregateRootId.new
-    expect(uuid_valid? aggregate_id.to_s ).not_to be nil
+    aggregate_id = SimpleEventSourcing::UUIDAggregateRootId.generate
+    expect(uuid_valid?(aggregate_id.to_s)).not_to be nil
   end
 
   it 'is created from a  UUID' do
-    aggregate_id = SimpleEventSourcing::UUIDAggregateRootId.new(value: 'cd2d7408-e230-49fa-a22b-51a004ecbec0')
-    expect(uuid_valid? aggregate_id.to_s ).not_to be nil
-    expect(aggregate_id.to_s ).to eq('cd2d7408-e230-49fa-a22b-51a004ecbec0')
+    aggregate_id = SimpleEventSourcing::UUIDAggregateRootId.new 'cd2d7408-e230-49fa-a22b-51a004ecbec0'
+    expect(uuid_valid?(aggregate_id.to_s)).not_to be nil
+    expect(aggregate_id.to_s).to eq('cd2d7408-e230-49fa-a22b-51a004ecbec0')
   end
 
+  it 'is fails if  string is no valid  UUID' do
+    expect(SimpleEventSourcing::UUIDAggregateRootId.new('foo')).to raise_error(AggregateRootIdError)
+  end
 end
