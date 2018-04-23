26f4c96ce5a24723bcd98407a0527229138baa63
Manel <mlopezt@prosodie.com>
2018-04-23 13:18:54 +0200

    Create History spec

--- spec/aggregate_root/base_spec.rb
+++ spec/aggregate_root/base_spec.rb
@@ -47,14 +47,5 @@ RSpec.describe SimpleEventSourcing::AggregateRoot::Base do
     expect(@dummy_class.events.count).to eq(0)
   end
 
-  it 'is reconstructed by a stream events' do
-    aggregate_id = SimpleEventSourcing::Id::UUIDId.new '4bb20d71-3002-42ea-9387-38d6838a2cb7'
-    stream_events = DummyStreamEvents.new(aggregate_id)
-    stream_events << DummyEvent.new(aggregate_id, 10)
-    stream_events << DummyEvent.new(aggregate_id, 20)
 
-    aggregate = stream_events.get_aggregate
-    expect(aggregate.aggregate_id.to_s).to eq('4bb20d71-3002-42ea-9387-38d6838a2cb7')
-    expect(aggregate.a_field).to eq(20)
-  end
 end
