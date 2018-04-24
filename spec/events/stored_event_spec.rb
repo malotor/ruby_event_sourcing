fab502db0ee13df3999245b31ed27a92c4cd8293
Manel <mlopezt@prosodie.com>
2018-04-24 17:03:57 +0200

    Add stored event spec

--- spec/events/stored_event_spec.rb
+++ spec/events/stored_event_spec.rb
@@ -3,30 +3,32 @@ RSpec.describe SimpleEventSourcing::Events::StoredEvent do
   before(:each) do
 
     @stored_event = SimpleEventSourcing::Events::StoredEvent.new(
-      aggregate_id: SimpleEventSourcing::Id::UUIDId.generate,
-      occurred_on: Time.now.getlocal("+02:00").to_i,
-      event_type: :DummyEvent,
+      aggregate_id: 'an_id',
+      occurred_on: 1402394400,
+      event_type: 'DummyEvent',
       event_data: '{"a_new_value":10}'
     )
   end
 
   it 'has occured on date' do
-    expect(@stored_event.occurred_on).not_to be nil
+    expect(@stored_event.occurred_on).to eq(1402394400)
   end
 
   it 'has a aggregate_id' do
-    expect(@stored_event.aggregate_id).not_to be nil
+    expect(@stored_event.aggregate_id).to eq('an_id')
   end
 
   it 'has as a type' do
-    expect(@stored_event.event_type).to eq(:DummyEvent)
+    expect(@stored_event.event_type).to eq('DummyEvent')
   end
 
-  it 'has as a type' do
+  it 'has as a data' do
     expect(@stored_event.event_data).to eq('{"a_new_value":10}')
   end
 
-
+  it 'is seriablizable' do
+    expect(@stored_event.serialize).to eq('{"aggregate_id":"an_id","occurred_on":1402394400,"event_type":"Dummy","event_data":{"a_new_value":10}}')
+  end
 
 
 end
